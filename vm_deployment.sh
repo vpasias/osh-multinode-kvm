d5eeaa5b445a963b06262e5fedd87d5e284a841d
vpasias <52623837+vpasias@users.noreply.github.com>
2022-11-12 18:56:03 +0200

    Update vm_deployment.sh

--- vm_deployment.sh
+++ vm_deployment.sh
@@ -178,6 +178,8 @@ ssh -o "StrictHostKeyChecking=no" ubuntu@n1 "echo 'gprm8350' > rpass && chmod 04
 
 ssh -o "StrictHostKeyChecking=no" ubuntu@n1 'ssh-keygen -t rsa -q -N ""'
 
+ssh -o "StrictHostKeyChecking=no" ubuntu@n1 "sshpass -f upass scp '-o StrictHostKeyChecking=no' ~/.ssh/id_rsa* ubuntu@n2:~/.ssh/id_rsa*"
+
 ssh -o "StrictHostKeyChecking=no" ubuntu@n1 "sshpass -f upass ssh-copy-id -o 'StrictHostKeyChecking=no' -i ~/.ssh/id_rsa.pub ubuntu@n2"
 ssh -o "StrictHostKeyChecking=no" ubuntu@n1 "sshpass -f upass ssh-copy-id -o 'StrictHostKeyChecking=no' -i ~/.ssh/id_rsa.pub ubuntu@n3"
 ssh -o "StrictHostKeyChecking=no" ubuntu@n1 "sshpass -f upass ssh-copy-id -o 'StrictHostKeyChecking=no' -i ~/.ssh/id_rsa.pub ubuntu@n4"
